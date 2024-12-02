b1f9a5a47fa921420b166a3445fede7e26f03058
operatorIO-stock <vova.chigorin@yandex.com>
2024-12-02 20:27:07 +0300

    easy prog

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgramPassword;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userPassword = "1234";
+        string userAnswer;
+
+        int numberOfAttempts = 0;
+
+        Console.WriteLine("Welcome to the Program\nEnter the password:");
+
+        while(numberOfAttempts < 3)
+        {
+            userAnswer = Console.ReadLine();
+
+            if(userAnswer != userPassword)
+            {
+                Console.WriteLine("Access denied");
+                numberOfAttempts++;
+            }
+            else
+            {
+                Console.WriteLine("Secret message...");
+            }
+        }
+    }
+}
